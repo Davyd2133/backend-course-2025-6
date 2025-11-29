2682d1163c0e36e7cf90e2b2270c05f7a563a1d2
Davyd2133 <hileckidavid@gmail.com>
2025-11-29 07:51:07 -0800

    add HTTP server

--- app.js
+++ app.js
@@ -15,3 +15,27 @@ const options = program.opts();
 console.log('Host:', options.host);
 console.log('Port:', options.port);
 console.log('Cache directory:', options.cache);
+
+const fs = require('fs');
+
+// Створюємо директорію кешу, якщо її не існує
+if (!fs.existsSync(options.cache)) {
+    fs.mkdirSync(options.cache, { recursive: true });
+    console.log("Cache directory created:", options.cache);
+}
+
+const http = require('http');
+
+// Створюємо HTTP-сервер
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Server is running');
+});
+
+// Запускаємо сервер
+server.listen(options.port, options.host, () => {
+    console.log(`Server running at http://${options.host}:${options.port}`);
+});
+
+
